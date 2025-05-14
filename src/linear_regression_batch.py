6d94b0612e1cb48b8969cf5675827eb7e1545558
Varun Bandaru <varun@aspr.ai>
2025-05-14 15:49:33 -0500

    Linear Regression

---
+++ src/linear_regression_batch.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+
+
+learning_rate = 0.01
+iterations = 100000
+training_split = 50
+
+data_frame = pd.read_csv("Iris.csv")
+data_frame = data_frame[data_frame["Species"] ==  "Iris-setosa"]
+data_frame = data_frame.drop(columns = ["Id"])
+
+plt.scatter(data_frame["SepalLengthCm"] , data_frame["SepalWidthCm"])
+
+total_data = [[[1 , data_frame["SepalLengthCm"].values.tolist()[i]], data_frame["SepalWidthCm"].values.tolist()[i]]for i in range(len(data_frame))]
+training_data = random.sample(total_data, training_split)
+testing_data = [data for data in total_data if data not in training_data]
+
+def gradientBatchDescent(hypothesis, training_data):
+    gradient = [0,0]
+    for(first, second) in training_data:
+        for i in range(len(hypothesis)):
+            gradient[i] += first[i] * (dotProduct(hypothesis , first) - second)
+    return [g/len(training_data) for g in gradient]
+
+def trainData(iterations, training_data):
+    hypothesis = [0,0]
+    for i in range(iterations):
+        gradient = gradientBatchDescent(hypothesis, training_data)
+        hypothesis = [hypothesis[i] - gradient[i] * learning_rate for i in range(len(gradient))]
+    return hypothesis
+
+
+def dotProduct(hypothesis, input):
+    result = 0
+    for i in range(len(input)):
+        result += hypothesis[i] * input[i]
+    return result
+
+
+linReg = trainData(iterations, training_data)
+model = linReg
+plt.plot([i for i in range(8)] , [dotProduct(model , [1 , i]) for i in range(8)])
+plt.show()
